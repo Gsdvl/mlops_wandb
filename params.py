fa656f0368859aeae46a5f2f5b229e23f1f2088d
Gsdvl <gsdvl2004@gmail.com>
2024-12-06 04:50:00 -0300

    Initial commit

---
+++ params.py
@@ -0,0 +1,5 @@
+WANDB_PROJECT = "mlops-course-001"
+ENTITY = None # set this to team name if working in a team
+BDD_CLASSES = {i:c for i,c in enumerate(['daisy', 'dandelion', 'rose', 'sunflower', 'tulip'])}
+RAW_DATA_AT = 'flowers_dataset'
+PROCESSED_DATA_AT = 'flowers_simples_split'
